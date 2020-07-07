665c3d4c0f87796779b7552fee42030bd8221498
joaogabrielmp <joaogabrielma@gmail.com>
2020-07-07 11:47:01 -0300

    up

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Image } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 import { Container, Title } from './styles';
 
@@ -11,6 +13,17 @@ const SignIn: React.FC = () => {
       <Image source={logoImg} />
 
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('A');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
