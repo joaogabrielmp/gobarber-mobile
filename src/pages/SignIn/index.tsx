d6ef541cc00c141992a77f3c9af68167a9238d60
joaogabrielmp <joaogabrielma@gmail.com>
2020-07-07 13:22:50 -0300

    up

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,30 +1,88 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  CreateAccountButton,
+  CreateAccountButtonText,
+  ForgotPassword,
+  ForgotPasswordText,
+  Title,
+} from './styles';
 
 const SignIn: React.FC = () => {
+  const [isHidden, setIsHidden] = useState(false);
+
+  const keyboardDidShow = useCallback((): void => setIsHidden(true), []);
+
+  const keyboardDidHide = useCallback((): void => setIsHidden(false), []);
+
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', keyboardDidShow);
+    Keyboard.addListener('keyboardDidHide', keyboardDidHide);
+
+    // cleanup function
+    return () => {
+      Keyboard.removeListener('keyboardDidShow', keyboardDidShow);
+      Keyboard.removeListener('keyboardDidHide', keyboardDidHide);
+    };
+  }, []);
+
   return (
-    <Container>
-      <Image source={logoImg} />
-
-      <Title>Faça seu logon</Title>
-
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
-
-      <Button
-        onPress={() => {
-          console.log('A');
-        }}
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
       >
-        Entrar
-      </Button>
-    </Container>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        />
+        <Container>
+          <Image source={logoImg} />
+
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
+
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button
+            onPress={() => {
+              console.log('A');
+            }}
+          >
+            Entrar
+          </Button>
+
+          <ForgotPassword onPress={() => {}}>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+        <ScrollView />
+      </KeyboardAvoidingView>
+
+      {!isHidden && (
+        <CreateAccountButton onPress={() => {}}>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>Criar um conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      )}
+    </>
   );
 };
 
