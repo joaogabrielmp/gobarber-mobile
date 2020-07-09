4b71aa59d73c8df886c49ed39ef473a9a65ecaae
joaogabrielmp <joaogabrielma@gmail.com>
2020-07-09 10:47:24 -0300

    up

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// import AppointmentCreated from '../pages/AppointmentCreated';
-// import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
+import CreateAppointment from '../pages/CreateAppointment';
 import Dashboard from '../pages/Dashboard';
-// import Profile from '../pages/Profile';
+import Profile from '../pages/Profile';
 
 const App = createStackNavigator();
 
@@ -18,10 +18,10 @@ const AppRoutes: React.FC = () => (
       },
     }}
   >
-    <App.Screen name="Dashboard" component={Dashboard} />
-    {/* <App.Screen name="CreateAppointment" component={CreateAppointment} />
     <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
-    <App.Screen name="Profile" component={Profile} />  */}
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
